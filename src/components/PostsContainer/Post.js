90ada43861c54c9559879d1bb5f932ae60aaa2c9
carlsachs <sachscarl@gmail.com>
2020-01-22 00:32:33 -0500

    finished mvp

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,6 +1,6 @@
 // You will add code in this file
 
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -8,8 +8,10 @@ import PostHeader from "./PostHeader";
 import "./Posts.css";
 
 const Post = props => {
-  // set up state for the likes
-
+  const [likes, setLikes] = useState(props.post.likes);
+  const addLikes = (e) => {
+    setLikes(likes => likes + 1);
+  }
   return (
     <div className="post-border">
       <PostHeader
@@ -25,7 +27,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likes={likes} addLikes={addLikes}/>
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,13 +4,19 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = props => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.post.map(p => (
+        <div>
+          <Post  post={p}/>
+        </div>
+  ))}
     </div>
   );
 };
+
 
 export default PostsPage;
 
